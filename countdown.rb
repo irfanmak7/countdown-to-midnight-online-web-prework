e1421be8117f131f99d07f3e1106685a1edc2437
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-08 01:19:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(integer)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer, second)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep second
+  end
+  "HAPPY NEW YEAR!"
+
+end
